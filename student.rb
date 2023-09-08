1e2d06241949442d11329557de50512a65637b9f
clarence <tegumclarence@gmail.com>
2023-09-08 13:53:59 +0100

    I added the test,rental,classromm and book files

--- student.rb
+++ student.rb
@@ -1,7 +1,16 @@
+require './person'
+
 class Student < Person
-  def initialize(classroom, age:, name: 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
